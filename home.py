c8432b0cb47a3cbde4e1513c98652e1ffbdd4a17
thanathon013 <120613765+thanathon013@users.noreply.github.com>
2023-12-15 14:49:05 +0700

    .commit

---
+++ home.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.header("Thanathon Niaoaongart")
+
+st.title('การทดสอบเขียนเว็บด้วย Python')
+st.header('นาย ธนธรณ์ เหนี่ยวองอาจ')
+st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
+st.markdown("----")
+
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    st.image('./pic/samakan.jpg')
+with col2:
+    st.image('./pic/ทานตะวัน.jpg')
+
+html_1 = """
+<div style="background-color:#FFA31E;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
+
+import pandas as pd
+
+dt=pd.read_csv('./data/iris.csv')
+st.write(dt.head(10))
